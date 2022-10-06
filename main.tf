018eea81e1d5aaa5645a53211b030fbff9a88e00
Maciob <macio99@gmail.com>
2022-10-06 16:24:24 +0200

    Probably wrong

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
 module "Compute"{
     source = "./Compute"
-    INSTANCES = var.INSTANCES
     Subnet_IDs = module.Network.Subnet_IDs
     SEC_GROUP_ID = module.Network.SEC_GROUP_ID
     VPC_GROUP_ID = module.Network.VPC_GROUP_ID
+
+    INSTANCES = var.INSTANCES
+    KEY_PATH = var.KEY_PATH
+    instance_type = var.instance_type
+    KEY_NAME = var.KEY_NAME
+    created_by = var.created_by
+    bootcamp = var.bootcamp
+    ami = var.ami
 }
 module "Network"{
     source = "./Network"
