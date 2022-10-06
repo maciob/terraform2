9833425e5ebf1f5d49fe0aa474506a05e0952e8a
Maciob <macio99@gmail.com>
2022-10-06 15:15:43 +0200

    Probably wrong

--- Compute/vars.tf
+++ Compute/vars.tf
@@ -1,20 +1,3 @@
-variable "AWS_REGION" {    
-    default = "eu-west-2"
-}
-variable "KEY_PATH"{
-    default = "/home/ubuntu/MaciejBekasKey.pem"
-}
-variable "VPC_NAME"{
-    default = "MaciejBekas-easy-vpc"
-}
-variable "INSTANCES"{
-    type = map(object({
-        az = string
-        ver = string
-        name = string
-        num = number
-    }))
-}
 variable "Subnet_IDs"{
     type = list(string)
 }
