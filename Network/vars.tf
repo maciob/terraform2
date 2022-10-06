413d1fa58cc8dd5d140ce6039ae96e9eb1df33d3
Maciob <macio99@gmail.com>
2022-10-06 15:37:13 +0200

    Probably wrong

--- Network/vars.tf
+++ Network/vars.tf
@@ -1,17 +1,6 @@
-variable "AWS_REGION" {    
-    default = "eu-west-2"
-}
-variable "KEY_PATH"{
-    default = "/home/ubuntu/MaciejBekasKey.pem"
-}
-variable "VPC_NAME"{
-    default = "MaciejBekas-easy-vpc"
-}
 variable "MY_SUBNETS"{
     type = map(object({
         cidr = string
         az = string
     }))
 }
-
-
