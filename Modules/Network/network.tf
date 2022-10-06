28d176b0bba34a909771d21d957a50f85695c675
Maciob <macio99@gmail.com>
2022-10-06 18:55:43 +0200

    Probably wrong

--- Modules/Network/network.tf
+++ Modules/Network/network.tf
@@ -28,10 +28,10 @@ resource "aws_security_group" "MaciejBekas-easy-sec-group" {
     dynamic "egress" {
         for_each = var.EGRESS
         content {
-            from_port = setting.value[each.value.from]
-            to_port = setting.value[each.value.to]
-            protocol = setting.value[each.value.prot]
-            cidr_blocks = setting.value[each.value.cidr]
+            from_port = EGRESS.value["from"]
+            to_port = EGRESS.value["to"]
+            protocol = EGRESS.value["prot"]
+            cidr_blocks = EGRESS.value["cidr"]
         }
     }
     dynamic "ingress" {
