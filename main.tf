7d6045ca8bf9d39db7e1406e92b8586ff792697c
Maciob <macio99@gmail.com>
2022-10-06 14:38:15 +0200

    Probably wrong

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "Compute"{
     source = "./Compute"
     INSTANCES = var.INSTANCES
+    Network = var.Network
 }
 module "Network"{
     source = "./Network"
     MY_SUBNETS = var.MY_SUBNETS
+    Compute = var.Compute
 }
